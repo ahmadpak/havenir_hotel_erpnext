a25cdb24cda3a61848af2e7b4f8d4b198a95eaa0
Muhammad Ahmad <ahmad@dev.mshome.net>
2020-02-06 11:51:41 +0500

    Bug fix: SI not generated for Non Walk in

--- havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/hotel_check_out/hotel_check_out.js
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/hotel_check_out/hotel_check_out.js
@@ -260,6 +260,16 @@ frappe.ui.form.on("Hotel Check Out", {
     }
     frm.trigger('total');
   },
+
+  is_tax_invoice: function(frm){
+    if (frm.doc.is_tax_invoice == 1){
+      frm.doc.naming_series = 'INV-CHK-OUT-.YYYY.-';
+    }
+    else{
+      frm.doc.naming_series = 'CHK-OUT-.YYYY.-';
+    }
+    frm.refresh_field('naming_series');
+  }
 });
 
 frappe.ui.form.on('Hotel Check Out Item', {
