27d67f80a68de88bf62d2eb88188700445fbac34
mohammadahmad1990 <mohammad.ahmad.zulfiqar1@gmail.com>
2020-04-23 15:04:23 +0500

    Templ Disabled Contact No Logic

--- havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/hotel_guests/hotel_guests.js
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/hotel_guests/hotel_guests.js
@@ -27,21 +27,21 @@ frappe.ui.form.on("Hotel Guests", {
         frappe.msgprint("Please enter CNIC in the correct format");
       }
     }
-    if (frm.doc.contact_no && frm.doc.contact_no.length != 12) {
-      frm.doc.contact_no = undefined;
-      frm.refresh_field("contact_no");
-      frappe.msgprint("Please enter Mobile No in the correct format");
-    } else {
-      if (frm.doc.contact_no[0] != "9") {
-        frm.doc.contact_no = undefined;
-        frm.refresh_field("contact_no");
-        frappe.msgprint("Please enter Mobile No in the correct format");
-      }
-      if (frm.doc.contact_no[1] != "2") {
-        frm.doc.contact_no = undefined;
-        frm.refresh_field("contact_no");
-        frappe.msgprint("Please enter Mobile No in the correct format");
-      }
-    }
+    // if (frm.doc.contact_no && frm.doc.contact_no.length != 12) {
+    //   frm.doc.contact_no = undefined;
+    //   frm.refresh_field("contact_no");
+    //   frappe.msgprint("Please enter Mobile No in the correct format");
+    // } else {
+    //   if (frm.doc.contact_no[0] != "9") {
+    //     frm.doc.contact_no = undefined;
+    //     frm.refresh_field("contact_no");
+    //     frappe.msgprint("Please enter Mobile No in the correct format");
+    //   }
+    //   if (frm.doc.contact_no[1] != "2") {
+    //     frm.doc.contact_no = undefined;
+    //     frm.refresh_field("contact_no");
+    //     frappe.msgprint("Please enter Mobile No in the correct format");
+    //   }
+    // }
   }
 });
